4f8cd826803a19a3b9e0848abca5de896ac258f0
Jason Ji <jimanproductions@gmail.com>
2023-12-02 14:31:20 +0100

    pushes sensor data

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 3001; // You can use any available port
+const fs = require('fs'); //read files
 
 
 const cors = require('cors'); //for cross origin access.
@@ -8,17 +9,30 @@ const cors = require('cors'); //for cross origin access.
 app.use(cors());
 
 // Static JSON data for testing
-const sensorData = {
-    timestamp: new Date().toISOString(),
-    humidity: 50.3,
-    pressure: 1012.1,
-    interior_temperature: 21.5,
-    altitude: 300
-};
+// const sensorData = {
+//     timestamp: new Date().toISOString(),
+//     humidity: 50.3,
+//     pressure: 1012.1,
+//     interior_temperature: 21.5,
+//     altitude: 300
+// };
 
 // GET route to send sensor data
 app.get('/weather', (req, res) => {
-    res.json(sensorData);
+    fs.readFile('data.json', 'utf8', (err, data) => {
+        if (err) {
+            console.error("Error reading file:", err);
+            return res.status(500).send("Error reading sensor data");
+        }
+        try {
+            const sensorData = JSON.parse(data);
+            res.json(sensorData);
+        } catch (parseErr) {
+            console.error("Error parsing JSON:", parseErr);
+            return res.status(500).send("Error parsing sensor data");
+        }
+    });
+    // res.json(sensorData);
 });
 
 // Start the server
